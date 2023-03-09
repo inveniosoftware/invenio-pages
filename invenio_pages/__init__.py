985430370e73b7550b3a5bad80b4c709ba0c3b3a
Karolina Przerwa <karolina.m.przerwa@gmail.com>
2023-03-09 17:16:41 +0100

    release: v3.0.1

--- invenio_pages/__init__.py
+++ invenio_pages/__init__.py
@@ -11,6 +11,6 @@
 from .ext import InvenioPages, InvenioPagesREST
 from .records.models import PageList, PageModel
 
-__version__ = "3.0.0"
+__version__ = "3.0.1"
 
 __all__ = ("__version__", "InvenioPages", "InvenioPagesREST", "PageModel", "PageList")
