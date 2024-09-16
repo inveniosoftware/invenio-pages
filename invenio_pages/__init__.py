8288e86a4daaa9a2c6021eac836246ab0bf23a31
Christoph Ladurner <christoph.ladurner@tugraz.at>
2024-09-16 21:09:43 +0200

    release: v4.1.1

--- invenio_pages/__init__.py
+++ invenio_pages/__init__.py
@@ -2,6 +2,7 @@
 #
 # This file is part of Invenio.
 # Copyright (C) 2015-2024 CERN.
+# Copyright (C) 2024 Graz University of Technology.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -11,6 +12,6 @@
 from .ext import InvenioPages, InvenioPagesREST
 from .records.models import PageList, PageModel
 
-__version__ = "4.1.0"
+__version__ = "4.1.1"
 
 __all__ = ("__version__", "InvenioPages", "InvenioPagesREST", "PageModel", "PageList")
