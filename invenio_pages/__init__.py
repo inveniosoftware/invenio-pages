b73578b0c84def0a61a6a5db0720a8f30e9f3cdf
Sam Arbid <samaa2@kth.se>
2025-10-21 14:01:03 +0200

    📦 release: v7.2.1

--- invenio_pages/__init__.py
+++ invenio_pages/__init__.py
@@ -3,6 +3,7 @@
 # This file is part of Invenio.
 # Copyright (C) 2015-2025 CERN.
 # Copyright (C) 2024-2025 Graz University of Technology.
+# Copyright (C) 2025 KTH Royal Institute of Technology.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -12,6 +13,6 @@
 from .ext import InvenioPages, InvenioPagesREST
 from .records.models import PageList, PageModel
 
-__version__ = "7.2.0"
+__version__ = "7.2.1"
 
 __all__ = ("__version__", "InvenioPages", "InvenioPagesREST", "PageModel", "PageList")
