9033b405f2c60a90952906a6d6382517dfaba04d
Christoph Ladurner <christoph.ladurner@tugraz.at>
2026-06-16 16:22:17 +0200

    release: v10.0.0

--- invenio_pages/__init__.py
+++ invenio_pages/__init__.py
@@ -8,6 +8,6 @@
 from .ext import InvenioPages, InvenioPagesREST
 from .records.models import PageList, PageModel
 
-__version__ = "9.0.0"
+__version__ = "10.0.0"
 
 __all__ = ("__version__", "InvenioPages", "InvenioPagesREST", "PageModel", "PageList")
