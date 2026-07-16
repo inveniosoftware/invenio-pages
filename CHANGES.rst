0c736241ea03dee94427f31cdcce5b752fceed19
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-07-16 15:50:27 +0200

    release: v10.0.1

--- CHANGES.rst
+++ CHANGES.rst
@@ -3,11 +3,17 @@
     SPDX-FileCopyrightText: 2024-2026 Graz University of Technology.
     SPDX-FileCopyrightText: 2025 KTH Royal Institute of Technology.
     SPDX-FileCopyrightText: 2026 Northwestern University.
+    SPDX-FileCopyrightText: 2026 TU Wien.
     SPDX-License-Identifier: MIT
 
 
 Changes
 =======
+
+
+Version v10.0.1 (released 2026-07-16)
+
+- chore(setup): migrate from setuptools to hatchling
 
 Version v10.0.0 (released 2026-06-16)
 
--- invenio_pages/__init__.py
+++ invenio_pages/__init__.py
@@ -1,6 +1,7 @@
 # SPDX-FileCopyrightText: 2015-2025 CERN.
 # SPDX-FileCopyrightText: 2024-2026 Graz University of Technology.
 # SPDX-FileCopyrightText: 2025 KTH Royal Institute of Technology.
+# SPDX-FileCopyrightText: 2026 TU Wien.
 # SPDX-License-Identifier: MIT
 
 """Static pages module for Invenio."""
@@ -8,6 +9,6 @@
 from .ext import InvenioPages, InvenioPagesREST
 from .records.models import PageList, PageModel
 
-__version__ = "10.0.0"
+__version__ = "10.0.1"
 
 __all__ = ("__version__", "InvenioPages", "InvenioPagesREST", "PageModel", "PageList")
